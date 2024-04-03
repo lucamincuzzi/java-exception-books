4f3b72f9a884d49cd18f1d5a5898207da3125b09
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-04-03 15:21:56 +0200

    add: Libro class - constructor

--- src/org/experis/learning/Libro.java
+++ src/org/experis/learning/Libro.java
@@ -6,4 +6,16 @@ public class Libro {
     private int nPagine;
     private String autore;
     private String editore;
+
+    // Costruttori
+
+    public Libro(String titolo, int nPagine, String autore, String editore) throws IllegalArgumentException {
+        if(titolo.isEmpty() || nPagine <= 0 || autore.isEmpty() || editore.isEmpty()){
+            throw new IllegalArgumentException("Dati non validi, riprovare");
+        }
+        this.titolo = titolo;
+        this.nPagine = nPagine;
+        this.autore = autore;
+        this.editore = editore;
+    }
 }
