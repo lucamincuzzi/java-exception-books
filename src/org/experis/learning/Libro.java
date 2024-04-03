1400becde1299ba3c3b8b05e494184594da3e46c
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-04-03 14:49:03 +0200

    add: Libro class - attributes

---
+++ src/org/experis/learning/Libro.java
@@ -0,0 +1,9 @@
+package org.experis.learning;
+
+public class Libro {
+    // Attributi
+    private String titolo;
+    private int nPagine;
+    private String autore;
+    private String editore;
+}
